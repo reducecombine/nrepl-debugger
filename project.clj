b41c5ca63dd6475b7e8ae6078ce6b30fe51fad38
vemv <vemv@users.noreply.github.com>
2019-01-06 05:46:01 +0100

    `lein new nrepl-debugger`

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject nrepl-debugger "0.1.0-SNAPSHOT"
+  :description "A minimalistic nrepl-based debugger."
+  :url "https://github.com/reducecombine/nrepl-debugger"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0"]])
